f7dc2c20628a314d820a5a50ca43177ad962f6d1
Ricard Dalmau <rdalmau@gmail.com>
2021-11-14 18:53:55 +0100

    triangle networks 6, perlin textures, sliders 3_1

--- perlin textures.js
+++ perlin textures.js
@@ -1,18 +1,19 @@
 let palette;
 let redraw = true;
-
+let canvas_w;
+let canvas_h;
+let canvas_factor = 0.6;
 function setup() {
 
-    w = min(windowWidth, windowHeight);
-    wx = w;
-    wy = w;
-    createCanvas(wx, wy);
+		canvas_w = windowWidth * canvas_factor;
+		canvas_h = windowHeight * canvas_factor;
+    createCanvas(canvas_w, canvas_h);
 
     background('white');
 
-    fill(random_color(palette))
+    //fill(random_color(palette))
     sliders_add(0.01, 0.05, 0.02, 0.01, "k");
-    sliders_add(1, 10, 3, 1, "grain");
+    sliders_add(1, 10, 6, 1, "grain");
     sliders_add(1, 500, 50, 5, "square_width");
     sliders_add(1, 10, 9, 1, "palette_id");
     
@@ -20,13 +21,14 @@ function setup() {
 
 function draw() {
 
-    palette = fixed_palette(palette_id);
-    
+    //palette = fixed_palette(palette_id);
+		//palette = ["03071e","370617","6a040f","9d0208","d00000","dc2f02","e85d04","f48c06","faa307","ffba08"];
+		palette = ["f94144","f3722c","f8961e","f9c74f","90be6d","43aa8b","577590"];
     if (redraw)  {
         clear();
         background(220);
-        for (let x = 0; x < windowWidth; x+= square_width) {
-            for (let y = 0; y < windowHeight; y+= square_width) {
+        for (let x = 0; x < canvas_w; x+= square_width) {
+            for (let y = 0; y < canvas_h; y+= square_width) {
                 perlin_square(x, y, x+square_width, y+square_width, random_color(palette), k, grain);
             }
         }
@@ -36,11 +38,11 @@ function draw() {
 }
 
 // box blau     31,122 - 60,103 - 230, 107
-function perlin_square(x1, y1, x2, y2, color, k, grain) {
-
+function perlin_square(x1, y1, x2, y2, color, noise_factor, pixel_size) {
+		let k = noise_factor;
     noStroke();
-    for(let i = x1; i < x2; i+=grain){
-        for(let j = y1; j < y2; j+=grain){
+    for(let i = x1; i < x2; i+=pixel_size){
+        for(let j = y1; j < y2; j+=pixel_size){
             var r = noise(i*k,j*k)
             var g = noise(i*k+1000,j*k+1000)
             var b = noise(i*k+2000,j*k+2000)
@@ -53,7 +55,7 @@ function perlin_square(x1, y1, x2, y2, color, k, grain) {
             g = map (g, 0, 1, green(color)-30, green(color)+30);
 
             fill(r, g, b)
-            rect(i,j,13)
+            rect(i,j,pixel_size)
         }
     }
 
---
+++ triangle_network_6.js
@@ -0,0 +1,134 @@
+"use strict";
+let sketch_name = "triangle_network_6";
+let canvas_size = 800;
+let arr_dots;
+let color_dots_1;
+let color_dots_2;
+let color_line_same_row;
+let color_line_other_row;
+let color_mode;
+let dots_strokeWeight = 10;
+let lines_strokeWeight = 20;
+
+
+let palette = ["335c67","fff3b0","e09f3e","9e2a2b","540b0e"];
+let palette_verds = ["d8f3dc","b7e4c7","95d5b2","74c69d","52b788","40916c","2d6a4f","1b4332","081c15"];
+let palette_blaus = ["03045e","023e8a","0077b6","0096c7","00b4d8","48cae4","90e0ef","ade8f4","caf0f8"];
+let palette_color_0;
+
+			  
+function setup() {
+
+    width = canvas_size;
+    height = canvas_size;
+    createCanvas(width, height);
+    
+    keyboard_image_name(sketch_name); // set default image save name
+
+    
+    sliders_add(2, 35, 10, 1, "rows");	// 0
+    sliders_add(2, 20, 15, 1, "rows_min");	
+    sliders_add(2, 35, 14, 1, "rows_max");	
+    sliders_add(0, 2, 1, 1, "noise_mode");	
+    sliders_add(0.001, 0.1, 0.05, 0.005, "perlin_k");	
+    
+	//palette = shuffle(palette);
+    palette_color_0 = "#" + palette[0];
+    palette = palette.slice(1, );
+
+}
+
+
+function draw() {
+
+    let ch = sliders_changed();
+    switch (ch) {
+        case "_first_":
+        case "rows":
+        case "rows_min":
+        case "rows_max":
+            arr_dots = grid_create(rows, rows_min, rows_max, width);
+		    // aixo es perque alguns triangles no es dibuixen be i almenys te fondo de la paletta
+		    fill(color(palette_color_0));
+		    square(0, 0, canvas_size); 
+        case "noise_mode":
+        case "perlin_k":
+        default:
+		    clear();
+            background(0);
+            grid_draw_triangles(arr_dots, noise_mode, perlin_k);
+    }
+
+}
+
+
+// a: 2d array of dots
+// draw triangles
+// noise_mode:  0 = no noise, 1 = perlin triangles (fast), 2 = perlin dot level (slow)
+// k: perlin noise factor
+function grid_draw_triangles(a, noise_mode=0, k=0.005) {
+
+
+    // lines to next rows
+    //stroke(color_line_other_row);  
+    noStroke();
+    strokeWeight(1); 
+    let triangle_color = 0;  
+    for (let row = 0; row < a.length - 1; row++) {              // all rows
+        for (let i = 0; i < a[row].length; i++ ) {              // all dots in current row
+
+            // triangles with vertex in current dot and base in neighbours
+
+            let dot_a = a[row][i];
+            for (let n = 1; n < a[row][i].neighbours.length; n++) {     // all neighbours for dot_a
+                // a triangle with vertex in A and base in two neigbours B & C
+                let dot_b =  a[row][i].neighbours[n-1];
+                let dot_c = a[row][i].neighbours[n];
+                //print(dot_a + " " + dot_b + " " + dot_c);
+                let col = fixed_color(triangle_color, palette_blaus);
+                triangle_color++;
+                switch (noise_mode) {
+                    case 1:
+                        perlin_triangle_2(dot_a.x, dot_a.y, dot_b.x, dot_b.y, dot_c.x, dot_c.y, col, k, 60, 12);
+                    break;
+                    case 2:
+                        perlin_triangle(dot_a.x, dot_a.y, dot_b.x, dot_b.y, dot_c.x, dot_c.y, col, k, 1, 30)
+                        break;
+                    case 0:
+                    default:                    
+                        fill(col);
+                        triangle(dot_a.x, dot_a.y, dot_b.x, dot_b.y, dot_c.x, dot_c.y);
+                }
+            }
+
+            // triangle with base in current row and vertex in neighbours 
+
+            if (i < a[row].length -1 && a[row][i].neighbours.length > 0  ) {
+                let dot_a = a[row][i];
+                let dot_b =  a[row][i+1];
+                let dot_c = a[row][i].neighbours[a[row][i].neighbours.length-1];
+
+                let col = fixed_color(triangle_color, palette_verds);
+                triangle_color++;
+                switch (noise_mode) {
+                    case 1:
+                        perlin_triangle_2(dot_a.x, dot_a.y, dot_b.x, dot_b.y, dot_c.x, dot_c.y, col, k, 60, 12)
+                        break;
+                    case 2:
+                        perlin_triangle(dot_a.x, dot_a.y, dot_b.x, dot_b.y, dot_c.x, dot_c.y, col, k, 1, 30)
+                        break;
+                    case 0:
+                    default:                    
+                        fill(col);
+                        triangle(dot_a.x, dot_a.y, dot_b.x, dot_b.y, dot_c.x, dot_c.y);
+                }
+            }
+
+
+        
+        }
+    }
+    
+}
+
+
